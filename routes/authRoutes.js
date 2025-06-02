a8c06a7e7d60e9d7397c925f0e6c59caa912c5a4
arjunv <arjunv.nxtgen.com>
2025-06-02 12:05:34 +0530

    made some changes in Todo

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const Users = require("../Schema/user");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = process.env.JWT_SECRET;
 require("dotenv").config();
 
 const router = express.Router();
@@ -25,6 +27,14 @@ router.post("/signup", async (req, res) => {
   const newUser = new Users({ username, password: hashed });
   await newUser.save();
 
+  const token = jwt.sign(
+    { username: newUser.username },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "24h",
+    }
+  );
+  res.cookie("token", token, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 });
   return res.status(201).json({ message: "Signup successful" });
 });
 
@@ -32,6 +42,7 @@ router.post("/signup", async (req, res) => {
 router.post("/login", async (req, res) => {
   const { username, password } = req.body;
   const user = await Users.findOne({ username });
+  res.cookie.user = username;
   if (user) {
     res.json({ message: "Login Successful" }).send(200);
   } else {
@@ -49,4 +60,5 @@ router.post("/login", async (req, res) => {
 
   return res.status(200).json({ message: "Login Successful", token });
 });
+
 module.exports = router;
